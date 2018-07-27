b048fc16c3190e1437f925e53f51c901aa4dc082
Yana Korotkina <yanaselin@gmail.com>
2018-07-27 17:09:31 +0300

    isPalindrome/refactor: remove exstra semicolon

--- src/isPalindrome.js
+++ src/isPalindrome.js
@@ -1,4 +1,4 @@
 module.exports = function isPalindrome(string = '') {
   const sample = string.toLowerCase().split('').filter(char => char !== ' ').join('');
-  return sample === sample.split('').reduceRight((acc, el) => acc + el, '');;
+  return sample === sample.split('').reduceRight((acc, el) => acc + el, '');
 };
