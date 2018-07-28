9ec4bbbc315fe40f40c5ee8cecb01123a1a0fd10
Yana Korotkina <yanaselin@gmail.com>
2018-07-28 03:12:54 +0300

    isBalanced/feat: custom brackets

--- src/isBalanced.js
+++ src/isBalanced.js
@@ -1,13 +1,35 @@
-module.exports = function isBalanced(string = '') {
+const defaultConfig = [['{', '}'], ['[', ']'], ['(', ')']];
+
+const searchInConfig = (bracket, config) => (
+  config.reduce((acc, el, i) => {
+    if (el[0] === bracket) {
+      return { isOpened: true, double: el[0] === el[1], index: i }
+    }
+    if (el[1] === bracket) {
+      return { isClosed: true, index: i }
+    }
+    return acc;
+  }, null)
+);
+
+module.exports = function isBalanced(string = '', config = defaultConfig) {
   const opened = [];
   const length = string.length;
 
   for (let i = 0; i < length; i += 1) {
-    if (string[i] === '{') {
-      opened.push('{');
+    const bracket = searchInConfig(string[i], config);
+    if (!bracket) {
       continue;
     }
-    if (string[i] === '}' && opened.pop() !== '{') {
+    const isDoubleOpened = bracket.double && opened.includes(bracket.index);
+    if (isDoubleOpened && opened.pop() !== bracket.index) {
+      return false;
+    }
+    if (bracket.isOpened && !isDoubleOpened) {
+      opened.push(bracket.index);
+      continue;
+    }
+    if (bracket.isClosed && opened.pop() !== bracket.index) {
       return false;
     }
   }
--- test/isBalanced.test.js
+++ test/isBalanced.test.js
@@ -1,6 +1,10 @@
 const assert = require('assert');
 const isBalanced = require('../src/isBalanced');
 
+const config1 = [['1', '2'], ['3', '4'], ['5', '6']];
+const config2 = [['(', ')'], ['|', '|']];
+const config3 = [['1', '2'], ['3', '4'], ['5', '6'], ['7', '7'], ['8', '8']];
+
 describe('isBalanced', () => {
   it('should work without arguments', () => {
     assert.equal(isBalanced(), true);
@@ -23,5 +27,23 @@ describe('isBalanced', () => {
     assert.equal(isBalanced('foo { bar { baz } boo }'), true);
     assert.equal(isBalanced('foo { bar } }'), false);
     assert.equal(isBalanced('foo { bar { baz }'), false);
+  });
+
+  it('should work with three types of brackets by default', () => {
+    assert.equal(isBalanced('(foo { bar (baz) [boo] })'), true);
+    assert.equal(isBalanced('foo { bar { baz }'), false);
+    assert.equal(isBalanced('foo { (bar [baz] } )'), false);
+  });
+
+  it('should accept custom brackets config as second argument', () => {
+    assert.equal(isBalanced('1134356422', config1), true);
+    assert.equal(isBalanced('1134356322', config1), false);
+  });
+
+  it('should work with brackets with same open and close characters', () => {
+    assert.equal(isBalanced('|()|', config2), true);
+    assert.equal(isBalanced('|(|)', config2), false);
+    assert.equal(isBalanced('11111561111112288222255778778775555666677776622222', config3), true);
+    assert.equal(isBalanced('55555575557777888866766113383484411112222342266666', config3), false);
   });
 });
